7f70c867732512bcc787716f3885fe62d99c4c2c
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2026-08-09 18:22:03 +0000

    fix: update webpack configuration for PDF handling and change build command to use webpack

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,14 @@ const nextConfig: NextConfig = {
 	turbopack: {
 		root: __dirname,
 	},
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.pdf$/i,
+			type: "asset/source",
+		});
+
+		return config;
+	},
 	async headers() {
 		return [
 			{
